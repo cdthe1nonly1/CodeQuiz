784632b58acc8b8a81efc03047705f3410933007
Charles Dykstra <charles.b.dykstra@gmail.com>
2022-11-03 23:06:08 -0600

    updated to change questions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# CodeQuiz
+# CodeQuiz# passwordGenerator
+
+# Quiz
+
+Create a Quiz site that keeps score.
+
+# Technologies
+
+1. HTML
+2. JAVA SCRIPT
+3. CSS
+
+# Links
+
+link to page:
+
+# Images
+
+# Usage
+
+Use to test knowledge.
+
+# Licences
+
+None
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,78 @@ var startBtn = document.getElementById("startButton");
 var introPage = document.getElementById("intro");
 var questionAreaElement = document.getElementById("question-area");
 
+var ans0CLick = document.getElementById("ans0");
+var ans1CLick = document.getElementById("ans1");
+var ans2CLick = document.getElementById("ans2");
+var ans3CLick = document.getElementById("ans3");
+
+var questionTracker = 0;
+
+// Array of questions to put into question area.
+var questionsArr = [
+  {
+    question: "History: On what day was Pearl Harbor attacked?",
+    choices: [
+      "December 7, 1941",
+      "June 22, 1941",
+      "October 6, 1939",
+      "September 11, 2001",
+    ],
+    answer: "December 7, 1941",
+  },
+  {
+    question: "Science: What is the 6th planet in our solar system?",
+    choices: ["Mars", "Jupiter", "Saturn", "Venus"],
+    answer: "Jupiter",
+  },
+  {
+    question: "English: ___ is the study of the meaning of languages",
+    choices: ["Phonetics", "Syntax", "Semantics", "Linquistics"],
+    answer: "Semantics",
+  },
+  {
+    question: "Social Studies: What do meterologists study?",
+    choices: ["Humans", "Animals", "Chemistry", "The Weather"],
+    answer: "The Weather",
+  },
+];
+//create a start button event listener
 startBtn.addEventListener("click", startGame);
 
+// event listner for answers
+ans0CLick.addEventListener("click", clickOnAnswer);
+ans1CLick.addEventListener("click", clickOnAnswer);
+ans2CLick.addEventListener("click", clickOnAnswer);
+ans3CLick.addEventListener("click", clickOnAnswer);
+
+// this starts the game
 function startGame() {
-  console.log("started");
   startBtn.classList.add("hide");
   introPage.classList.add("hide");
   questionAreaElement.classList.remove("hide");
+  goToNextQuestion();
 }
 
-function goToNextQuestion() {}
+function goToNextQuestion() {
+  var title = document.getElementById("questions");
+  var answerOne = document.getElementById("ans0");
+  var answerTwo = document.getElementById("ans1");
+  var answerThree = document.getElementById("ans2");
+  var answerFour = document.getElementById("ans3");
 
-function clickOnAnswer() {}
+  var currentQuestion = questionsArr[questionTracker];
+  title.textContent = currentQuestion.question;
+  answerOne.textContent = currentQuestion.choices[0];
+  answerTwo.textContent = currentQuestion.choices[1];
+  answerThree.textContent = currentQuestion.choices[2];
+  answerFour.textContent = currentQuestion.choices[3];
+}
 
-// var questions = [
-//   {
-//     question: "what is my name",
-//     choices: ["matt", "dyland", "bob", "sal"],
-//     answer: "matt",
-//   },
-//   {
-//     question: "what is my name",
-//     choices: ["matt", "dyland", "bob", "sal"],
-//     answer: "matt",
-//   },
-// ];
+function clickOnAnswer() {
+  console.log("test click on answer function");
+}
+
+//function setNextQuestion() {}
 
 // var index = 0;
 
